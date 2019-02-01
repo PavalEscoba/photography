d872a3ae9ac9c151ddd745c156caff014170ada4
Paval Miatlitski <paval.escoba@gmail.com>
2019-02-01 19:07:57 +0300

    new version 1.1

---
+++ scripts/authors.js
@@ -0,0 +1,54 @@
+const container = document.querySelector('.container');
+const createTable = document.createElement('table');
+createTable.id = 'info-table';
+createTable.classList.add('table');
+createTable.classList.add('table-striped');
+
+const currentProfile = data;
+
+let body = '';
+currentProfile.forEach((element, i) => {
+  body += `
+    <tr class="authorNumber_${element.id}">
+      <th scope="row">${i + 1}</th>
+      <td><a class="link" href="./photographer_${i + 1}.html">${element.name}</a></td>
+      <td>${element.dateOfBirth}</td>
+      <td>${element.placeOfBirth.city}</td>
+    </tr>
+  `;
+});
+
+createTable.innerHTML = `
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">#</th>
+      <th id='table-fullname' scope="col">ФИО</th>
+      <th id='table-dateOfBirth' scope="col">Дата рождения</th>
+      <th id='table-placeOfBirth' scope="col">Место Рождения</th>
+    </tr>
+  </thead>
+  ${body}`;
+container.appendChild(createTable);
+
+function tableSearch() {
+  const phrase = document.getElementById('js-search-input');
+  const table = document.getElementById('info-table');
+  const regPhrase = new RegExp(phrase.value, 'i');
+  let flag = false;
+  for (let i = 1; i < table.rows.length; i++) {
+    flag = false;
+    for (let j = table.rows[i].cells.length - 1; j >= 0; j--) {
+      flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
+      if (flag) break;
+    }
+    if (flag) {
+      table.rows[i].style.display = '';
+    } else {
+      table.rows[i].style.display = 'none';
+    }
+  }
+}
+
+document.querySelector('#js-search-input').addEventListener('keyup', () => {
+  tableSearch();
+});
---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+$(() => {
+  const infoArray = data;
+  const randomNum = (min, max) => {
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
+  };
+  (() => {
+    const randomInteger = randomNum(0, infoArray.length - 1);
+    const author = infoArray[randomInteger];
+    const picture = document.querySelector('.author-of-the-day__pic');
+    const name = document.querySelector('.author-of-the-day__name');
+    const info = document.querySelector('.author-of-the-day__text');
+    const link = document.querySelector('.author-of-the-day__link');
+    picture.setAttribute('src', author.mainPhoto);
+    name.textContent = author.name;
+    info.textContent = author.generalInfo;
+    link.setAttribute('href', `./photographer_${author.id}.html`);
+  })();
+});
